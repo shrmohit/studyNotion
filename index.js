816fc70e3c75b19dcc27d8e3549d2f7313d82198
mohit sharma <mohitsharma7258@gmail.com>
2025-06-06 22:43:33 +0530

    server run

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+require('dotenv').config();
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running at port http://localhost${PORT}`);
+});
