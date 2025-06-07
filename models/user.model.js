f4246745e9d44352a9d3721b536ab96025ed15a3
mohit sharma <mohitsharma7258@gmail.com>
2025-06-07 23:40:55 +0530

    usermodel

---
+++ models/user.model.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    requied: true,
+  },
+  lastName: {
+    type: String,
+    requied: true,
+  },
+  email: {
+    type: String,
+    requied: true,
+  },
+  password: {
+    type: String,
+    requied: true,
+  },
+  confirm: {
+    type: String,
+    requied: true,
+  },
+  phoneNumber: {
+    type: String,
+    requied: true,
+  },
+});
