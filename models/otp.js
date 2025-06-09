6540d7889f51384ec63d5e76f6a8a0f77627320e
mohit sharma <mohitsharma7258@gmail.com>
2025-06-10 00:25:42 +0530

    otp model

---
+++ models/otp.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const otpSchema = mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  otp: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+    expires: 5 * 60,
+  },
+});
