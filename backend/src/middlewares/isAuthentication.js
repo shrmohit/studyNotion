520d98158f451acde78a9530ff58ca043e2d92ce
mohit sharma <mohitsharma7258@gmail.com>
2025-06-15 17:48:08 +0530

    middleware auth

---
+++ backend/src/middlewares/isAuthentication.js
@@ -0,0 +1,19 @@
+import jwt from 'jsonwebtoken';
+
+export const isAuthentication = async (req, res, next) => {
+  try {
+    const token = req.cookies?.token;
+    if (!token) {
+      return res.status(401).json({
+        message: 'Access denied',
+        success: false,
+      });
+    }
+
+    const decode = jwt.verify(token, process.env.SECRET_KEY);
+    req.user = decode;
+    next();
+  } catch (error) {
+    console.log('Error in authentication', error);
+  }
+};
